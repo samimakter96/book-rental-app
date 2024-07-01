f4bc20c819de283ec83f9121ecc37179f9023f2c
samim akter <samimakter9641@gmail.com>
2024-07-01 15:12:50 +0530

    initial commit with tailwind css and react router dom added

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+
+      <h1 className='text-7xl text-green-400'>Book Rentals App</h1>
+    </div>
+  )
+}
+
+export default App
